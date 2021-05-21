0cfcb7815ce179ab110ac7e4704b68a6c677d2aa
Nathan Drouillard <72321102+ndrouillard@users.noreply.github.com>
2021-05-21 10:54:45 -0400

    May13v3

--- Feb28.py
+++ Feb28.py
@@ -1,12 +1,10 @@
 """
-Created on Mon May 17 2021
+Created on Fri May 21 2021 (most up-to-date script)
 @author: Nathan Drouillard, modified from BitScope_GUI_fast.py
 A special thanks is owed to Aananth Kanagaraj (Codenio)
 Notes: -use BL_STREAM mode in bitlib_read_data_for_GUI
        -the stage functions are from "micronix_functions(Feb21).py"
-"""
-from bitlib import *
-from bitscope import *
+"""from bitlib import *
 import numba
 import tkinter as tk
 import tkinter.font as font #this is needed but unused?
@@ -30,7 +28,6 @@ from threading import Thread
 root = tk.Tk()
 root.geometry("1024x768")
 root.title("FTIR GUI")
-
 myFont = tk.font.Font(family='Helvetica', size=15, weight='bold')
 
 #%% Major variables
@@ -42,7 +39,19 @@ FALSE = 0
 #bitscope_setup(scanRate,Nt)
 myFont = ("Helvetica",12)
 now = str(datetime.now()) #this is for naming the csv file by the date
-#%%
+
+#%%Variables for data collection
+global t_vec, y, w_vec, yw_vec, all_t_vec, all_y, all_w_vec, all_yw_vec
+tic = time.time()
+t_vec1 = np.empty(1)
+y1 = np.empty(1)
+w_vec1 = np.empty(1)
+yw_vec1 = np.empty(1)
+all_t_vec = np.empty(1)
+all_y = np.empty(1)
+all_w_vec = np.empty(1)
+all_yw_vec = np.empty(1)
+old_time = 0
 
 #%% Micronix Stuff
 c = 3e8
@@ -51,107 +60,7 @@ neglim = -1e-2
 d = poslim + np.abs(neglim)
 #optical_t = np.linspace(0,d/c,Nt)
 
-#Variables for data collection
-global t_vec, y, w_vec, yw_vec, all_t_vec, all_y, all_w_vec, all_yw_vec
-tic = time.time()
-t_vec = np.empty(1)
-y = np.empty(1)
-w_vec = np.empty(1)
-yw_vec = np.empty(1)
-all_t_vec = np.empty(1)
-all_y = np.empty(1)
-all_w_vec = np.empty(1)
-all_yw_vec = np.empty(1)
-old_time = 0
-
-#Plotting stuff
-fig_time = Figure(figsize=(5,4))
-ax_time = fig_time.add_subplot(111)
-canvas_time = FigureCanvasTkAgg(fig_time, master=root)
-
-f = Figure(figsize=(5,5),dpi=100)#, tight_layout=True)
-f2 = Figure(figsize=(5,5),dpi=100)
-#f3 = Figure(figsize=(5,5),dpi=100)
-#d = f3.add_subplot(111)
-a = f.add_subplot(111)
-b = f2.add_subplot(111)
-g = Figure(figsize=(5,5),dpi=100)
-c = g.add_subplot(111)
-
-canvas = FigureCanvasTkAgg(f, root)
-canvas2 = FigureCanvasTkAgg(f2, root)
-
-#%% Open the BitScope
-ser = serial.Serial('/dev/ttyUSB_MICRONIX',38400)
-#%% Setup the Bitscope
-#def bitscope_setup():
-
- #   global MY_SIZE, MY_RATE, TRUE, FALSE
-    #Setup general parameters for the capture
-
-print("Starting: Attempting to open one devices...")
-
-#Attempt to open 1 device at /dev/ttyUSBx
-#Make sure you run 'cd /dev/' followed by 'ls ' on terminal to see if the device is present.
-
-# initialisation of scope collects all the device data and stores them in scope.devices list
-# and closes open devices on object termination
-scope = Scope('USB:/dev/ttyUSB1',1)
-
-#See return value to see the number of successfully opened devices.
-if (scope.device_count==0):
-    print("  FAILED: all devices not found (check your probe file).")
-else:
-    #Successfully opened one device
-    #Report the number of devices opened, and the library version used
-    print('\nNumber of devices opened: %s' %scope.device_count)
-    print(" Library: %s (%s)\n\n" %(scope.version(VERSION.LIBRARY),scope.version(VERSION.BINDING)))
-
-    # collect the list of devices
-    devices = scope.devices
-
-    #Select the first device opened, found at location 0 by default.
-    #Setup acquisition in FAST mode, where the whole of the 12288 samples in
-    #the buffer are used by one channel alone.
-    devices[0].mode(MODE.FAST)
-    #devices[0].mode(MODE.STREAM) #when this is used, it defaults to ~4kHz for some reason
-
-    #Report the capture details
-    print(" Capture: %d @ %.0fHz = %fs" % (scope.tracer.size(),scope.tracer.rate(MY_RATE),scope.tracer.time()))
-
-    #Setup channel-nonspecific parameters for capture.
-    scope.tracer.configure(
-        rate=MY_RATE, # optional, default BL_MAX_RATE
-        size=MY_SIZE, # optional default BL_MAX_SIZE
-        pre_capture=ZERO, #How many seconds to capture before the trigger event- 0 by default
-        post_capture=ZERO, #How many seconds to capture after the trigger event- 0 by default
-    )
-
-    #Set up channel A properties - A has channel index 0, B has 1.
-    #All the subsequent properties belong to channel A until another is selected.
-
-    #Setup a falling-edge trigger at 0.999V.
-    #Other options are BL_TRIG_RISE, BL_TRIG_HIGH, BL_TRIG_LOW.
-    scope.tracer.trigger(0.999,TRIGGER.FALL)
-
-    # select channel 0 form device 0 and configure
-    devices[0].channels[0].configure(
-        source=SOURCE.BNC, # use the POD input - the only one available (I tried putting BNC here, to no avail)
-        offset=ZERO, # Y-axis offset is set to zero as BL_ZERO
-        range=devices[0].channels[0].analog_range_count, # maximum range for y-axis - use this whenever possible
-        coupling=COUPLING.DC
-    )
-
-    #Enable the currently selected channel, i.e. channel A
-    #This ensures the recorded data goes into the memory-buffer in Bitscope device
-    devices[0].channels[0].enable()
-
-    #Capture analog data synchronously to the Bitscope device's buffer.
-    #If a trigger event is not received in 0.1sec, auto trigger happens.
-    #BL_Trace(), when without any arguments, captures immediately, no trigger needed.
-    print("trace {}".format(scope.tracer.trace(0.01,TRACE.SYNCHRONOUS)))
-
-#%% Functions
+#%% Home the stage
 def home():
 
     ser.write(b'1HOM\r')
@@ -215,75 +124,130 @@ def close():
 
     ser.close()
 
-def move():
+#%% Setup the Bitscope
+#%% Open the COM port
+ser = serial.Serial('/dev/ttyUSB_MICRONIX',38400)
+def bitscope_setup():
 
-    for i in range(0,4):
-        # ser.write(b'1PGL0\r') #loop program continuously
-        ser.write(b'1MLN\r') #move to negative limit
-        time.sleep(1)
-        #print("moved neg")
-        # ser.write(b'1MVA-2\r')
-        ser.flush()
-        ser.write(b'1WST\r')
-        time.sleep(1)
-        ser.flush()
-        #movedneg = ser.readline()
-        #ser.flush()
-        #print(movedneg)
-        # ser.write(b'1WTM5000\r') #wait for 5000 ms
+    global MY_SIZE, MY_RATE, TRUE, FALSE
+    #Setup general parameters for the capture
 
-        ser.write(b'1MLP\r') #move to positive limit
-        # ser.write(b'1MVA2\r')
-        time.sleep(1)
-        ser.flush()
-        ser.write(b'1WST\r')
-        time.sleep(1)
-        ser.flush()
-        run_Fourier() #take FFT
-        #movedpos = ser.readline()
-        #ser.flush()
-        #print("movedpos")
+    print("Starting: Attempting to open one devices...")
 
-        ser.write(b'1MLN\r') #move to negative limit
-        time.sleep(1)
-        #print("moved neg")
-        # ser.write(b'1MVA-2\r')
-        ser.flush()
-        ser.write(b'1WST\r')
-        time.sleep(1)
-        ser.flush()
+    #Attempt to open 1 device at /dev/ttyUSBx
+    #Make sure you run 'cd /dev/' followed by 'ls ' on terminal to see if the device is present.
 
-        run_Fourier() #take FFT
-        #movedneg = ser
+    #See return value to see the number of successfully opened devices.
+    if (BL_Open('USB:/dev/ttyUSB_BITSCOPE',1)==0):
+    #if (BL_Open('USB:/dev/ttyUSB_BITSCOPE',1)==0):
+        print("  FAILED: all devices not found (check your probe file)."    )
+    else:
+        #Successfully opened one device
+        #Report the number of devices opened, and the library version used
+        #print('\nNumber of devices opened: %s' ,BL_Count(BL_COUNT_DEVICE))
+        print(" Library: %s (%s)\n\n" , (BL_Version(BL_VERSION_LIBRARY),BL_Version(BL_VERSION_BINDING)))
+
+        #Select the first device opened, found at location 0 by default.
+        BL_Select(BL_SELECT_DEVICE,0)
+
+        #Setup acquisition in FAST mode, where the whole of the 12288 samples in
+        #the buffer are used by one channel alone.
+        BL_Mode(BL_MODE_FAST)
+        #BL_Mode(BL_MODE_STREAM)
+
+        #Report the capture details
+        print(" Capture: %d @ %.0fHz = %fs" , (BL_Size(),BL_Rate(MY_RATE),BL_Time()))
+
+        #Setup channel-nonspecific parameters for capture.
+        BL_Intro(BL_ZERO); #How many seconds to capture before the trigger event- 0 by default
+        BL_Delay(BL_ZERO); #How many seconds to capture after the trigger event- 0 by default
+        BL_Rate(MY_RATE); # optional, default BL_MAX_RATE
+        BL_Size(MY_SIZE); # optional default BL_MAX_SIZE
+
+        #Set up channel A properties - A has channel index 0, B has 1.
+        #All the subsequent properties belong to channel A until another is selected.
+        BL_Select(BL_SELECT_CHANNEL,0);
+
+        #Setup a falling-edge trigger at 0.999V.
+        #Other options are BL_TRIG_RISE, BL_TRIG_HIGH, BL_TRIG_LOW.
+        BL_Trigger(0.999,BL_TRIG_FALL); # This is optional when untriggered BL_Trace() is used
+        #BL_Trigger(4)
+
+        BL_Select(BL_SELECT_SOURCE,BL_SOURCE_POD); # use the POD input - the only one available
+        #BL_Range(BL_Count(BL_COUNT_RANGE)); # maximum range for y-axis - use this whenever possible
+        BL_Range(12)
+        BL_Offset(BL_ZERO); # Y-axis offset is set to zero as BL_ZERO
+        #BL_Coupling(BL_COUPLING_DC);
+        BL_Enable(TRUE);
+        print("Bitscope enabled")
 
 #Record data from Bitscope and move stage
 def record():
-    global DATA, DATA1
+
     global MY_RATE, MY_SIZE, TRUE, FALSE, old_time
-    global t_vec, y, w_vec, yw_vec, tic, all_t_vec, all_y, all_w_vec, all_yw_vec
+    global t_vec1, y1, w_vec1, yw_vec1, tic, all_t_vec, all_y, all_w_vec, all_yw_vec
 
-    #bitscope_setup()
+    params()
+    time.sleep(1)
+    bitscope_setup()
 
-    while True:
-    #Transfer the captured data to our PC's memory using the USB link
+    for i in range(0,5):
         DATA1 = np.empty(1)
-        DATA = devices[0].channels[1].acquire()
-        data = np.array(DATA)
+        DATA2 = np.empty(1)
 
-        y = np.append(DATA1[1:],data)
+        ser.write(b'1MLN\r') #move to negative limit
+        time.sleep(1)
+        print("moved neg")
+
+        ser.flush()
+        ser.write(b'1MLP\r') #move to positive limit
+        time.sleep(1)
+        ser.flush()
+        data1 = BL_Acquire()
+        data1_array = np.array(data1)
+        print("movedpos")
+        y1 = np.append(DATA1,data1_array)
         tmin = old_time
         tmax = time.perf_counter()
         old_time = tmax
-        t_vec = np.linspace(tmin,tmax,len(y))
-
-        all_y = np.append(all_y, y)
-        all_t_vec = np.append(all_t_vec, t_vec)
-        all_w_vec = np.append(all_w_vec, w_vec)
-        all_yw_vec = np.append(all_yw_vec, yw_vec)
-
+        t_vec1 = np.linspace(tmin,tmax,len(y1))
+        dt = t_vec1[1]-t_vec1[0]
+        w_vec1 = 2*np.pi/(2*dt)*np.linspace(-1,1,len(y1))
+        dw = w_vec1[1]-w_vec1[0]
+        yw_vec1 = np.fft.fftshift(np.fft.fft(np.fft.fftshift(y1)))
+        all_y = np.append(all_y, y1)
+        all_t_vec = np.append(all_t_vec, t_vec1)
+        all_w_vec = np.append(all_w_vec, w_vec1)
+        all_yw_vec = np.append(all_yw_vec, yw_vec1)
+        #plot()
         count = len(all_yw_vec)
         data_count_disp.config(text=count)
+        
+        ser.write(b'1MLN\r') #move to negative limit
+        time.sleep(1)
+        print("moved neg")
         #data_count_disp.after(int(1e-6),record)
+        data2 = BL_Acquire()
+        data2_array = np.array(data2)
+        print("movedpos")
+        y2 = np.append(DATA2,data2_array)
+        tmin = old_time
+        tmax = time.perf_counter()
+        old_time = tmax
+        t_vec2 = np.linspace(tmin,tmax,len(y1))
+        dt = t_vec1[1]-t_vec1[0]
+        w_vec2 = 2*np.pi/(2*dt)*np.linspace(-1,1,len(y2))
+        dw2 = w_vec2[1]-w_vec2[0]
+        yw_vec2 = np.fft.fftshift(np.fft.fft(np.fft.fftshift(y2)))
+        all_y = np.append(all_y, y2)
+        all_t_vec = np.append(all_t_vec, t_vec2)
+        all_w_vec = np.append(all_w_vec, w_vec2)
+        all_yw_vec = np.append(all_yw_vec, yw_vec2)
+        #plot()
+        count = len(all_yw_vec)
+        data_count_disp.config(text=count)
+
+    #return (t_vec, y, w_vec, yw_vec)
 
 data_count_disp = tk.Label(root, font = myFont)
 data_count_disp.place(x=750, y=600)
@@ -291,30 +255,15 @@ data_count_disp.place(x=750, y=600)
 data_count_text = tk.Label(root, text = 'Data Points Processed', font = myFont)
 data_count_text.place(x=575, y=600)
 
-def Fourier():
-    global DATA, DATA1
-    global MY_RATE, MY_SIZE, TRUE, FALSE, old_time
-    global t_vec, y, w_vec, yw_vec, tic, all_t_vec, all_y, all_w_vec, all_yw_vec
-
-    dt = t_vec[1]-t_vec[0]
-    w_vec = 2*np.pi/(2*dt)*np.linspace(-1,1,len(y))
-    dw = w_vec[1]-w_vec[0]
-    yw_vec = np.fft.fftshift(np.fft.fft(np.fft.fftshift(y)))
-
-    all_w_vec = np.append(all_w_vec, w_vec)
-    all_yw_vec = np.append(all_yw_vec, yw_vec)
-
 #%% Threads
 def run_record():
-    params()
     t1 = threading.Thread(target = record)
     t1.start()
-    t0 = threading.Thread(target = move)
-    t0.start()
 
-def run_Fourier():
-    t3 = threading.Thread(target = Fourier)
-    t3.start()
+def run_plot():
+    t2 = threading.Thread(target = plot)
+    t2.start()
+#%%
 
 #Write data to file
 def write_to_file():
@@ -334,27 +283,37 @@ def write_to_file():
     df.to_csv (file, sep=',', index = False, header=True)
 
     print("Data saved")
-    print(len(all_t_vec))
-    print(len(all_y))
-    print(len(all_w_vec))
-    print(len(all_yw_vec))
 
 #%% Plotting stuff
+fig_time = Figure(figsize=(5,4))
+ax_time = fig_time.add_subplot(111)
+canvas_time = FigureCanvasTkAgg(fig_time, master=root)
 
+f = Figure(figsize=(5,5),dpi=100)#, tight_layout=True)
+f2 = Figure(figsize=(5,5),dpi=100)
+#f3 = Figure(figsize=(5,5),dpi=100)
+#d = f3.add_subplot(111)
+a = f.add_subplot(111)
+b = f2.add_subplot(111)
+g = Figure(figsize=(5,5),dpi=100)
+c = g.add_subplot(111)
+
+canvas = FigureCanvasTkAgg(f, root)
+canvas2 = FigureCanvasTkAgg(f2, root)
 
 #Plot data
 def plot():
 
-    global t_vec, y, w_vec, yw_vec
+    global t_vec1, y1, w_vec1, yw_vec1
 
     a.cla()
-    a.plot(y,'.-')
+    a.plot(t_vec1,y1,'.-')
     a.set_xlabel('Time (s)')
     a.set_ylabel('Voltage (V)')
-    a.set_ylim([0,2])
+    a.set_ylim([0,12])
 
     b.cla()
-    b.plot(w_vec,np.abs(yw_vec))
+    b.plot(w_vec1,np.abs(yw_vec1))
     b.set_xlabel('Frequency (Hz)')
     b.set_ylabel('Intensity (a.u.)')
 
@@ -403,7 +362,7 @@ button_home_stage = tk.Button(root,
 button_home_stage.place(x=100,y=580)
 
 button_record = tk.Button(root, #this button is an edited version of the start_stage_button from FTIR_GUI_Thorlabs(Final).py
-    text="Start Scan",
+    text="Start Bitscope",
     width = 10,
     height = 2,
     command = lambda: [run_record()],#, data_count()],
@@ -422,13 +381,13 @@ button_save.place(x=850, y = 580)
 
 button_quit = tk.Button(root,
     text="Quit",
-    command = lambda: [root.destroy(), scope.close()],
+    command = lambda: [root.destroy()],#, close_BitScope()],
     width = 10,
     height = 2,
     bg = "red",
     fg = "black",
     font = myFont,
     )
-button_quit.place(x=850,y=630)
+button_quit.place(x=850,y=640)
 
 root.mainloop()
